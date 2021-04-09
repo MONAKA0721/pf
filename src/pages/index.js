31d7b911153ca7a26888dc5c95d2544c58f0e71e
Yuya Sumie <monamonacan0926@gmail.com>
2021-04-10 04:06:31 +0900

    setup top page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,64 @@
+import AppBar from "@material-ui/core/AppBar";
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import IconButton from '@material-ui/core/IconButton';
+import { createMuiTheme, createStyles, makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { Link } from 'gatsby';
-import * as React from "react";
+import MenuIcon from '@material-ui/icons/Menu';
+import { StaticImage } from "gatsby-plugin-image";
+import * as React from 'react';
 
-// markup
+const useStyles = makeStyles(() =>
+  createStyles({
+    toolbar: {
+      justifyContent: "flex-end",
+    }
+  })
+);
+
 const IndexPage = () => {
+  const classes = useStyles();
   const theme = createMuiTheme({
     palette: {
       primary: {
-        light: '#b6ffff',
-        main: '#81d4fa',
-        dark: '#4ba3c7',
-        contrastText: '#FFFFFF',
+        main: "#191919",
       },
-      secondary: {
-        light: '#ff93c1',
-        main: '#ef6091',
-        dark: '#b92a64',
-        contrastText: '#000000',
+      background: {
+        default: "#212121",
       },
+      text: {
+        primary: "#FFFFFF",
+      },
+    },
+    typography: {
+      fontFamily: [
+        '"Times"',
+        'serif',
+      ].join(','),
     },
   });
   
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar className={classes.toolbar}>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
       <Container maxWidth="sm">
         <Box my={4}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            example
+          <Typography color="textPrimary" variant="h3" component="h1" gutterBottom>
+            Yuya Sumie
           </Typography>
-          <Link to="/about">
-            Go to the about page
-          </Link>
+          <Typography color="textPrimary" variant="h5" component="h1" gutterBottom>
+            - Portfolio Site -
+          </Typography>
+          <StaticImage src="../images/hero.png" alt="Snow landscape"/>
         </Box>
       </Container>
     </ThemeProvider>
